a6bcd2f6083d9f3d5b90b108e95b6072a514b7e3
linimasatua <155327934+linimasatua@users.noreply.github.com>
2024-02-05 04:11:43 +0700

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,19 +1,43 @@
 const { exec } = require('child_process')
 
-let process = null
+const SYMBLE = '$'
+const LENGTH = 5
 
-connect()
+let SIZE = 0
+let EXTRA = '0'
 
-async function connect() {
-    process = exec('node root.js')
+let process01 = null
 
-    process.stdout.on('data', (data) => {
+process.argv.slice(2).forEach(function (data, index) {
+    try {
+        if (index == 0) {
+            let size = parseInt(data)
+        
+            SIZE = (size-1)*LENGTH
+
+            console.log('★★★---START---★★★')
+
+            connect01()
+        } else if(index == 1) {
+            if (data == '1' || data == 1) {
+                EXTRA = '1'
+            }
+        }
+    } catch (error) {
+        console.log('Index Error:', error)
+    }
+})
+
+async function connect01() {
+    process01 = exec('node root.js '+(SIZE+1)+' '+EXTRA)
+
+    process01.stdout.on('data', (data) => {
         let log = data.toString().trimStart().trimEnd()
         if (log.length > 0) {
-            console.log(log.trim())
+            console.log(log)
         }
-        if(data.toString().includes('---EXIT---')) {
-            connect()
+        if(data.toString().includes(SYMBLE+SYMBLE+'---EXIT----')) {
+            connect01()
         }
     })
 }
